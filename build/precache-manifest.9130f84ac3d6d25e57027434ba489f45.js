e066116c3724e2298d20c183d00f529805e16623
Steffy Lo <steffy-lo@hotmail.com>
2020-05-13 16:22:54 -0400

    finalize

---
+++ build/precache-manifest.9130f84ac3d6d25e57027434ba489f45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e529027b83111b2027f0fde419d53d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ff7cc9097136d85c9f",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4915409c8188ac92c7cc",
+    "url": "/static/css/main.d1484f20.chunk.css"
+  },
+  {
+    "revision": "32ff7cc9097136d85c9f",
+    "url": "/static/js/2.8f3dc171.chunk.js"
+  },
+  {
+    "revision": "65b2bbc1257185d4add917f400c10925",
+    "url": "/static/js/2.8f3dc171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4915409c8188ac92c7cc",
+    "url": "/static/js/main.819bd713.chunk.js"
+  },
+  {
+    "revision": "2702132ab11494f38705",
+    "url": "/static/js/runtime-main.da9e5a3f.js"
+  },
+  {
+    "revision": "5bfd96d35093f78f4b676e2f0dca6cd1",
+    "url": "/static/media/logoTitle.5bfd96d3.png"
+  },
+  {
+    "revision": "eabb8b64ccd84b043d3ef8b7cfb88b00",
+    "url": "/static/media/settings.eabb8b64.png"
+  }
+]);

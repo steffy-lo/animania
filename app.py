ba08dc666d7c21c4e20fcc50922189716f522421
Steffy Lo <steffy-lo@hotmail.com>
2020-05-13 21:11:42 -0400

    v2

--- app.py
+++ app.py
@@ -96,16 +96,17 @@ def get_model_recommendations():
         anime_list = user["anime_list"]
 
         global get_similar_users
-        update_list = (username in recommendations["user"]) \
-                      and recommendations["user"][username][1] != anime_list
-        if get_similar_users is None or update_list:
+
+        if get_similar_users is None:
             get_similar_users = Thread(target=similar_users, args=(username,))
             get_similar_users.start()
-            time.sleep(25)
             return jsonify({'result': 'processing'})
 
-        # wait for thread to finish
-        get_similar_users.join()
+        update_list = (username in recommendations["user"]) and recommendations["user"][username][1] != anime_list
+        if update_list:
+            if get_similar_users is not None and get_similar_users.is_alive():
+                return jsonify({'result': 'processing'})
+
         if username not in recommendations["user-based"]:
             user_based_recommendation(username, k, n)
         else:

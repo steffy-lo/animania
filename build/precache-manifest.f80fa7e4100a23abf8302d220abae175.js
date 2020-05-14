bb8307a3576e195b834298da74b4f10da2e16100
Steffy Lo <steffy-lo@hotmail.com>
2020-05-13 23:45:48 -0400

    fixed UI bugs with React

---
+++ build/precache-manifest.f80fa7e4100a23abf8302d220abae175.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755c9c55e4f24eeb3b6a93db7af7ae76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ff7cc9097136d85c9f",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fadaed1c85de7a0625b7",
+    "url": "/static/css/main.af8e46da.chunk.css"
+  },
+  {
+    "revision": "32ff7cc9097136d85c9f",
+    "url": "/static/js/2.8f3dc171.chunk.js"
+  },
+  {
+    "revision": "65b2bbc1257185d4add917f400c10925",
+    "url": "/static/js/2.8f3dc171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fadaed1c85de7a0625b7",
+    "url": "/static/js/main.adc1a7d8.chunk.js"
+  },
+  {
+    "revision": "2702132ab11494f38705",
+    "url": "/static/js/runtime-main.da9e5a3f.js"
+  },
+  {
+    "revision": "5bfd96d35093f78f4b676e2f0dca6cd1",
+    "url": "/static/media/logoTitle.5bfd96d3.png"
+  },
+  {
+    "revision": "eabb8b64ccd84b043d3ef8b7cfb88b00",
+    "url": "/static/media/settings.eabb8b64.png"
+  }
+]);
